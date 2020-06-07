44b6f0c2d9ec2bc6f00a29f2ca989f1e7b68d009
yuma_kawabata <kawabata@iotbase.co.jp>
2020-06-08 08:06:32 +0900

    use activate deactivated

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,5 +10,11 @@ export default {
   destroyed: function() {
     console.log("destroyed");
   },
+  deactivated: function() {
+    console.log("deactiveted");
+  },
+  activated: function() {
+    console.log("activeted");
+  },
 };
 </script>
