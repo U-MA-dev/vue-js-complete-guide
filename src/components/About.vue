885544a030d2745a432f53ea2f46d57bd43641af
yuma_kawabata <kawabata@iotbase.co.jp>
2020-06-08 08:03:12 +0900

    destroyed dynamic components

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,14 @@
 <template>
-  <p>About</p>
+  <div>
+    <p>About</p>
+    <input type="text" />
+  </div>
 </template>
+
+<script>
+export default {
+  destroyed: function() {
+    console.log("destroyed");
+  },
+};
+</script>
